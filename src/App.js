b60590a421391e3dda882af2d30cc27ca18d9565
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-02-05 15:52:46 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -9,15 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type="button" onClick={  }>Reverse</button>
+
+        <p>
+        <button type="button" onClick={}>Reverse</button>
+        </p>
       </header>
     </div>
   );
