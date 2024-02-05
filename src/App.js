c74a280a2220d2e98a28cb180c9e716d6725211f
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-02-05 16:15:03 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import { useState } from 'react';
 function App() {
 
   //useState
+  //para inversao do giro da logo
   const [reverse, setReverse] = useState(false);
   const reverseClass = reverse ? 'reverse' : '';
-  const handleClick  = () => setReverse(!reverse)
+  const handleClick  = () => {
+    setReverse(!reverse);
+    setCounters(!reverse);
+  }
+  //----------------------------------------
+//para contar os clicks no button
+  const [counters, setCounters]=useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>Contador: { count }</h1>
+        <h1>Contador: { counters }</h1>
         <p>
         <button type="button" onClick={ handleClick }>Reverse {reverseClass} </button>
         </p>
