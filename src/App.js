13444ece5cbdaba3f9a4deec3271b346f7f6a544
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-02-23 16:40:35 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import P from 'prop-types'
 import './App.css';
 import { useMemo } from 'react';
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 
 function App() {
+  const [posts, setPosts] = useState([]);
   console.log('Componente pai renderizado!');
+
   //component did mount
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts');
-  },[ ]);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((r) => r.json());
+    .then((r) => setPosts(r));
+  }, []);
+
   return (
     <div className='App'>
       <h1>teste</h1>
