d20df6283991800f5476087b9a88f703461b7bf6
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-03-05 13:48:51 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -11,4 +11,5 @@ export default App;
 
 
 //para entender primeiro sobre esse hook sera preciso entender o q é contexto
+//é quando tem um componente, q dentro deste tem varios outros, para especificar outras atividades
 //
