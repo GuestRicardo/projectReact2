e88c8e7757c65d05a42294e3ea0079b83d81b01f
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-03-05 13:51:20 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -12,4 +12,8 @@ export default App;
 
 //para entender primeiro sobre esse hook sera preciso entender o q é contexto
 //é quando tem um componente, q dentro deste tem varios outros, para especificar outras atividades
-//
+//então seria varias propriedades descendo de niveis
+//nesses casos nao é inviavel nesta situação, para isso q precisa do useCotext
+//pq nao e ideal passar varias p´rops ate chegar o componente idela q deseja realizar a ação
+
+
