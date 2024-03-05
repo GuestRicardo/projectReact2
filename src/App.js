61268e0cc45b10054055783975204a8ffbe6269b
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-03-05 14:43:33 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './App.css';
 import { useContext } from 'react';
+import { useState } from 'react';
 
 //estado inicial(objeto)=
 //para passar para os componentes será preciso usar props
@@ -35,11 +36,15 @@ const Body = () => {
 //fim dos componentes
 
 function App() {
+
+  //mudando o estado
+  const [contextState, setContextstate] = useState(globalState);
+
   return (
     //usando o estado
     //dentro do estado esta os componentes
     //apartir do momento q recebe o valor o contexto será este no momento
-    <GlobalContext.Provider value={globalState}>
+    <GlobalContext.Provider value={{contextState, setContextstate}}>
       <Div />
     </GlobalContext.Provider>
   );
@@ -53,7 +58,7 @@ export default App;
 //nesses casos nao é inviavel nesta situação, para isso q precisa do useCotext
 //pq nao e ideal passar varias p´rops ate chegar o componente idela q deseja realizar a ação
 //para isso é criado o estado global,
-//para passar o contexto sera via estado, e com a propriedade Provider, o q os componentes veem para controlar sera o value(valor)
+//para passar o contexto sera o estado inicial, e com a propriedade Provider, o q os componentes veem para controlar sera o value(valor)
 // q será passado pelo contexto, entao resulmo, o contexto recebe o estado para q o componente possa o ver
 //com useContext esta sendo pegado elementos do filho sem preisar de props de forma direta, mo caso o titulo q esta no estado
 //isso significa q podemos pegar dados direto de qualquer contexto
