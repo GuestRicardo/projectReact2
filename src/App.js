dc8b38a03be7b5dc120a42f1318e787fde42a5ac
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-03-12 15:30:40 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { useReducer } from 'react';
+import { useReducer, createContext } from 'react';
 import './App.css';
 
-//estado Complexo
-const globalState = {
+//simulando os locais
+//data.js
+export const globalState = {
   title: 'Estado Complexo',
   body: 'o useReducer e muito similar ao useState so q ele foi projetado para trabalhar com estados complexos',
   counter: 0
 };
+//AppContext.js
+export const Context = createContext();
+export const AppContext = ({children })=>{
+  return
+}
 
 function App() {
   return (
