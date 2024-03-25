47f0cd2f7bda5921081647bc4ea9fa0b6b5f5b27
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-03-25 14:39:24 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -55,10 +55,12 @@ export const H1 = () => {
   const inputRef = useRef();
 
   return (
-    <h1 onClick={() => context.changeTitle( inputRef.current.value )}>
-      {context.state.title}
-    </h1>
-    <input type="text" ref={inputRef} />
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
+        {context.state.title}
+      </h1>
+      <input type="text" ref={inputRef} />
+    </>
   );
 }
 //componente fim
