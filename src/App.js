ff2862cce066793a46d54585f9a050d7e7cc0eb9
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-03-26 14:58:07 -0300

    formatando

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import P from 'prop-types'//esse proptypes é um react node
 import { useReducer, createContext, useContext, useRef, useEffect, useState } from 'react';
 import './App.css';
 
+//criando um hook, ele é uma função no react
 //ele vai simular o setInterval
-const useMyHook = () => {
-
+const useMyHook = (callback) => {
+  //esta função stInterval esta na API do proprio node
+  //esta sendo executado a cada 2 segundos
+  useEffect(() => {
+    const interval = setInterval(() => {
+      callback();
+    }, 2000);
+    //lembrando q é sempre certo limpar o lixo da memoria, para isso usa o clearInterval
+    return () => clearInterval(interval);
+  }, [callback]);
 };
 
 //App.jsx
 function App() {
   //estado
-
   const [counter, setCounter] = useState(0);
-  useEffect(() => {
-    //esta função stInterval esta na API do proprio node
-    //esta sendo executado a cada 2 segundos
-    const interval = setInterval(() => {
-      setCounter((c) => c + 1);
-    }, 2000);
-  }, []);
+
+  //usando o hook
+  useMyHook(() => {
+    setCounter((c) => c + 1);
+  });
+
   return (
     <div>
       <h1>Contador:{counter}</h1>
@@ -26,4 +33,3 @@ function App() {
   );
 }
 export default App;
-//criando um hook, ele é uma função no react
