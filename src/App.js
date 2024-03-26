77719a8773fcec9f00da4c63abd60faa0e269719
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-03-26 14:51:50 -0300

    formatandol

--- src/App.js
+++ src/App.js
@@ -11,18 +11,19 @@ const useMyHook = () => {
 function App() {
   //estado
 
-const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
   useEffect(() => {
     //esta função stInterval esta na API do proprio node
-    const interval = setInterval();
-  },[])
-  return (
-
-    <div>
-      <h1>Teste</h1>
-    </div>
-
-  );
+    //esta sendo executado a cada 2 segundos
+    const interval = setInterval(()=>{
+      setCounter((c) => c + 1 );
+    }, 2000);
+  }, []);
+ return (
+  <div>
+    <h1>Contador:{counter}</h1>
+  </div>
+);
 }
 export default App;
 //criando um hook, ele é uma função no react
