d2998bc6b63d9dbb2eccfc89f834cd5d6cedb1c8
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-04-09 23:43:59 -0300

    FORMATANDO

--- src/context/CouterProvider/reducer.js
+++ src/context/CouterProvider/reducer.js
@@ -1,1 +1,17 @@
-import * as types  from './Types';
+import * as types from './types';
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case types.INCREMENT_COUNTER: {
+      return { ...state, counter: state.counter + 1
+      }
+    }
+    case types.DECREMENT_COUNTER: {
+      return { ...state, counter: state.counter + 1
+      }
+    }
+  }
+  return {
+    ...state
+  };
+}
