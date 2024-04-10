15070020de47b642738380397f3d36295e2d8e1a
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-04-10 08:55:17 -0300

    formatando

--- src/context/CouterProvider/action.js
+++
@@ -1,8 +0,0 @@
-import * as types  from '../actions/types';
-
-export const incrementCounter = (dispatch) => {
-  dispatch({ type: types.INCREMENT_COUNTER, payload: });
-};
-export const decrementCounter = (dispatch) => {
-  dispatch({ type: types.DECREMENT_COUNTER, payload: });
-};
