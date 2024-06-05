c3d2cd155def2ab457bae7b0be26da90aa88af61
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-06-05 15:36:46 -0300

    formatando

--- src/components/estudo/index.jsx
+++ src/components/estudo/index.jsx
@@ -1,4 +1,6 @@
-export const Home = () =>{
-  const  [contador, ]
-  return
-}
+import { useState } from 'react';
+
+export const Home = () => {
+  const [contador, setContador] = useState([0, 1, 2, 3, 4]);
+  return <p>Teste</p>;
+};
