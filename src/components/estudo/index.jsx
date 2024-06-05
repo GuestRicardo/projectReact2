ebaeee36e26584433d709e6bfdf0a0f972522092
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-06-05 16:55:25 -0300

    formatando

--- src/components/estudo/index.jsx
+++ src/components/estudo/index.jsx
@@ -2,5 +2,23 @@ import { useState } from 'react';
 
 export const Home = () => {
   const [contador, setContador] = useState([0, 1, 2, 3, 4]);
-  return <p>Teste</p>;
+
+  const handleClick = () => {
+    setContador((c) => [...c, +c.slice(-1) + 1]);
+    {
+      /* o sinal de +(operador) é praconverter em string) */
+    }
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}> Contador {contador.slice(-1)}</button>
+      {/* esta pegando o ultimo elemento do array */}
+      <div>
+        {contador.map((c) => {
+          return <p key={`contando -${c}`}>{c}</p>;
+        })}
+      </div>
+    </>
+  );
 };
