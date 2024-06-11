31139fb43a703d66e0381638d2a2cf01db98e4e1
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-06-11 14:24:11 -0300

    formatando

--- src/components/estudo/index.jsx
+++ src/components/estudo/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export const Home = () => {
   const [contador, setContador] = useState([0, 1, 2, 3, 4]);
@@ -7,14 +7,16 @@ export const Home = () => {
     setContador((c) => [...c, +c.slice(-1) + 1]);
     //divRef é a referencia da div q esta no return com o scroll
     //entao ela esta referenciando diretamente do DOM
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const divRef = useRef();
     {
       /* o sinal de +(operador) é praconverter em string) */
     }
   };
 
-  /**Este useEffect é responsavel, para toda vez que atulaizar ela vai rolar na altura que estiver selecionado o objeto*/
-  useEffect(() => {
+  /**Este useLayoutEffect executará o codigo somente quando o navegador carregar na tela,
+   *  ele renderizara as coisas junto com o DOM ele vai esperar o DOM ser atualizado*/
+  useLayoutEffect(() => {
     divRef.current.scrollTop = divRef.current.scrollHeight;
   });
 
@@ -22,7 +24,7 @@ export const Home = () => {
     <>
       <button onClick={handleClick}> Contador {contador.slice(-1)}</button>
       {/* esta pegando o ultimo elemento do array */}
-      <div ref={ divRef }>
+      <div ref={divRef}>
         {contador.map((c) => {
           return <p key={`contando -${c}`}>{c}</p>;
         })}
