45680dcaad43b76974f80f0c1f821973fb4e217f
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-06-11 13:56:16 -0300

    formatando

--- src/components/estudo/index.jsx
+++ src/components/estudo/index.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef, useState } from 'react';
 
 export const Home = () => {
   const [contador, setContador] = useState([0, 1, 2, 3, 4]);
 
   const handleClick = () => {
     setContador((c) => [...c, +c.slice(-1) + 1]);
+    const divRef = useRef();
     {
       /* o sinal de +(operador) é praconverter em string) */
     }
   };
+  useEffect(() => {
+    divRef.current.scrollTop = divRef.current.scrollHeight;
+  });
 
   return (
     <>
